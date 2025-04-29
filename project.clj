8e0845b22a23e853b8f74cf691f954bc91b2f632
Nicolas Venegas <nic.venegas@gmail.com>
2025-04-29 21:41:41 +1000

    Upgrade kondo dep & fix missing org.clojure/tools.reader dep

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
 
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/tools.logging "1.3.0"]
+                 [org.clojure/tools.reader "1.5.2"]
                  [ch.qos.logback/logback-classic "1.3.15"]
-                 [clj-kondo "2024.11.14"]]
+                 [clj-kondo "2025.04.07"]]
 
   :aliases {"kondo" ["run" "-m" "clj-kondo.main" "--lint" "src:test" "--parallel"]})
