bdbc98ff3cb5fde109adf7cce796a70a676c72a0
Nicolas Venegas <nic.venegas@gmail.com>
2025-05-07 08:32:56 +1000

    Transducerify sizes

--- src/factorhouse/kafka/topic.clj
+++ src/factorhouse/kafka/topic.clj
@@ -5,21 +5,32 @@
 (defn sizes
   "Transform raw topic information into a more useable sizes shape"
   [topics]
-  (->> topics
-       (map (fn [[broker dirs]]
-              (map (fn [[dir {:keys [replica-infos error]}]]
-                     (when (= error "NONE")
-                       (map (fn [[{:keys [topic partition]} stats]]
-                              (merge {:broker broker
-                                      :dir dir
-                                      :topic topic
-                                      :partition partition}
-                                     stats))
-                            replica-infos)))
-                   dirs)))
-       flatten
-       (filter some?)
-       (sort-by (juxt :broker :dir :topic :partition))))
+  (sort-by
+   (juxt :broker :dir :topic :partition)
+   (sequence
+    (comp (map (fn [[broker dirs]]
+                 (map (fn [[dir {:keys [replica-infos error]}]]
+                        (when (= error "NONE")
+                          (map (fn [[{:keys [topic partition]} stats]]
+                                 (merge {:broker broker
+                                         :dir dir
+                                         :topic topic
+                                         :partition partition}
+                                        stats))
+                               replica-infos)))
+                      dirs)))
+          (mapcat flatten)
+          (filter some?))
+    topics)))
+
+(comment
+  (let [topics {1 {"/kfk" {:replica-infos {{:topic "br.ch" :partition 1}
+                                           {:size 2838281 :offset-lag 0 :future? false}}
+                           :error "NONE"}}
+                2 {"/kfk" {:error "Could not connect"}}}]
+   (sizes topics))
+  ;;
+  )
 
 ;; Extension Challenge! Implement these functions.
 
